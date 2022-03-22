a66f2e948f99b094805c3195636a6ac7f472d2fe
carys <caryspritc@gmail.com>
2022-03-22 11:51:54 +0000

    added marker

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,15 +9,30 @@ import * as L from 'leaflet';
 export class MapComponent implements AfterViewInit {
   private map: any;
   private bounds: any;
+  private marker: any;
   private image: any;
+  private LightIcon: any;
+  private defaultHmRadius = 40;
+  private defaultHmBlur = 25;
+  private gradient = {0.4: 'blue', 0.55: 'lime', 0.80: 'yellow', 1: 'red'}
 
   private initMap(): void {
     this.map = L.map('map', { crs: L.CRS.Simple });
     this.bounds = [[0,0],[1000,1000]];
     this.image = L.imageOverlay('assets/floor.jpg', this.bounds).addTo(this.map);
+  //   this.LightIcon = L.Icon.extend({
+  //     options: {
+  //     iconSize:[60, 55],
+  //     }
+  // });
+  //   this.LightIcon = new this.LightIcon ({iconUrl: 'assets/light.jpg'});
     this.map.fitBounds(this.bounds);
+    // this.heat = L.heatLayer([], {radius: this.defaultHmRadius, blur: this.defaultHmBlur, gradient:this.gradient }).addTo(this.map);
 
+this.marker = L.marker([800,800]).addTo(this.map);
   }
+
+  
 
   constructor() { }
 
